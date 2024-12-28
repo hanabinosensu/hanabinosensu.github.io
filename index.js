81cf7d649d5d01821ccb123a79f5b2242b1f1e55
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2024-12-28 13:01:23 +0900

    added floating action button to jump to #stickers

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 /*
  * Make #nav bar opaque when it hits the top border of the viewport, or
- * to put it differently, when the #notice banner leave the viewport.
+ * in other words, when #notice banner leaves the viewport.
  */
-const onintersection = (entries, _) => {
+new IntersectionObserver((entries, _) => {
   const nav = document.getElementById("nav");
-  const entry = entries[0];
 
-  if (entry.isIntersecting) {
+  if (entries[0].isIntersecting) {
     nav.classList.add("transparent");
   } else {
     nav.classList.remove("transparent");
   }
-}
+}, {}).observe(document.getElementById("notice"));
 
-const observer = new IntersectionObserver(onintersection, {});
-observer.observe(document.getElementById("notice"));
+/* Show #goto-stickers button when #schedule section enters viewport */
+new IntersectionObserver((entries, _) => {
+  if (entries[0].isIntersecting) {
+    document.getElementById("goto-stickers").classList.add("shown");
+  }
+}, {}).observe(document.getElementById("schedule"));
+
+/*
+ * Hide #goto-stickers button when upper 200px of the #stickers section
+ * becomes visible.
+ */
+new IntersectionObserver((entries, _) => {
+  if (entries[0].isIntersecting) {
+    document.getElementById("goto-stickers").classList.remove("shown");
+  }
+}, { rootMargin: "0px 0px -200px 0px" }).observe(document.getElementById("stickers"));
