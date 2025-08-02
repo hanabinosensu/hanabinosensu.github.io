cdf9ff765b4bf339c2d735137b47b7f6d8c62080
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2025-08-02 19:14:27 +0900

    removed quizzes because they are somewhat off-topic

--- index.js
+++ index.js
@@ -24,30 +24,3 @@ mustseeSelect.addEventListener("change", _ => {
 /* Recommend video randomly */
 mustseeSelect.selectedIndex = Math.floor(Math.random() * mustseeSelect.length);
 mustseeSelect.dispatchEvent(new Event("change"));
-
-/* Let users to go to next or previous quiz */
-// I don't like % operator because it returns negative value if n < 0. In fact,
-// % returns remainder rather than modulo.
-const mod = (n, m) => ((n % m) + m) % m;
-
-document.querySelectorAll("#quiz article").forEach((quiz, i, quizzes) => {
-  const prev = document.createElement("button");
-  prev.type = "button";
-  prev.classList.add("prev");
-  prev.addEventListener("click", _ => {
-    quiz.classList.remove("on");
-    quizzes.item(mod(i - 1, quizzes.length)).classList.add("on");
-  });
-
-  const next = document.createElement("button");
-  next.type = "button";
-  next.classList.add("next");
-  next.addEventListener("click", _ => {
-    quiz.classList.remove("on");
-    quizzes.item(mod(i + 1, quizzes.length)).classList.add("on");
-  });
-
-  const h3 = quiz.querySelector("h3");
-  h3.appendChild(prev);
-  h3.appendChild(next);
-});
