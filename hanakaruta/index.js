28df3d75e58a185846ba6e63273cc5a634428a4a
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2025-11-10 22:03:10 +0900

    added dedicated page for hanakaruta

---
+++ hanakaruta/index.js
@@ -0,0 +1,10 @@
+/* Show #nav bar only after #unofficial banner leaves the viewport */
+new IntersectionObserver((entries, _) => {
+  const nav = document.getElementById("nav");
+
+  if (entries[0].isIntersecting) {
+    nav.classList.remove("on");
+  } else {
+    nav.classList.add("on");
+  }
+}, {}).observe(document.getElementById("unofficial"));
