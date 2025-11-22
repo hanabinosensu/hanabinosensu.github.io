07e7663a1c4df6df68748787dd11ea2bd7e5a0d8
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2025-11-22 16:36:36 +0900

    added #pickup section

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ if (!navigator.languages.includes("ja")) {
   document.getElementById("i18n").style.display = "block";
 }
 
+/* Infinite #pickup slideshow */
+document.getElementById("pickup").scrollTo(0, 0);
+
+document.querySelectorAll("#pickup li").forEach((li, i, lis) => {
+  li.addEventListener("animationend", _ => {
+    const pickup = document.getElementById("pickup");
+    const next = lis.item((i + 1) % lis.length);
+
+    pickup.scrollTo({ top: next.offsetTop, behavior: "instant" });
+    next.getAnimations().forEach(animation => animation.play());
+  });
+});
+
+document.querySelector("#pickup li").getAnimations().forEach(animation => {
+  animation.play();
+});
+
 /* Let users to choose video for #mustsee */
 const mustseeSelect = document.getElementById("mustsee-select");
 
