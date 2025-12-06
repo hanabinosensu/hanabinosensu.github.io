f33cdd45c31c445c65ee0a7b924b6bb23f10906c
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2025-12-06 19:57:30 +0900

    renamed #pickup to #topics

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-/* Show #nav bar after #pickup leaves the viewport */
-new IntersectionObserver(([pickup, ..._]) => {
+/* Show #nav bar after #topics leaves the viewport */
+new IntersectionObserver(([topics, ..._]) => {
   const nav = document.getElementById("nav");
 
-  if (pickup.isIntersecting) {
+  if (topics.isIntersecting) {
     nav.classList.remove("on");
   } else {
     nav.classList.add("on");
   }
-}, {}).observe(document.getElementById("pickup"));
+}, {}).observe(document.getElementById("topics"));
 
 /* Show #i18n banner if user does not understand Japanese at all */
 if (!navigator.languages.includes("ja")) {
   document.getElementById("i18n").style.display = "block";
 }
 
-/* Infinite #pickup slideshow */
-document.getElementById("pickup").scrollTo(0, 0);
+/* Infinite #topics slideshow */
+document.getElementById("topics").scrollTo(0, 0);
 
-document.querySelectorAll("#pickup li").forEach((li, i, lis) => {
+document.querySelectorAll("#topics li").forEach((li, i, lis) => {
   li.addEventListener("animationend", _ => {
-    const pickup = document.getElementById("pickup");
+    const topics = document.getElementById("topics");
     const next = lis.item((i + 1) % lis.length);
 
-    pickup.scrollTo({
-      top: next.offsetTop - pickup.offsetTop,
+    topics.scrollTo({
+      top: next.offsetTop - topics.offsetTop,
       behavior: "instant",
     });
 
@@ -31,7 +31,7 @@ document.querySelectorAll("#pickup li").forEach((li, i, lis) => {
   });
 });
 
-document.querySelector("#pickup li").getAnimations().forEach(animation => {
+document.querySelector("#topics li").getAnimations().forEach(animation => {
   animation.play();
 });
 
