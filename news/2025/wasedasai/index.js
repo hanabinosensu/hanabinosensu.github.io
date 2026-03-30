9ce80162445a70850ef88fb8bb66bdafb4f5dffd
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2026-03-30 19:17:54 +0900

    use matchMedia to detect window resize

--- news/2025/wasedasai/index.js
+++ news/2025/wasedasai/index.js
@@ -1,18 +1,16 @@
 /* Show #nav iff window height >= 420px and #unofficial is outside viewport */
-const debounce = f => { let t; return () => { clearTimeout(t); t = setTimeout(f, 50); }; };
-
-window.addEventListener("resize", debounce(() => {
+window.matchMedia("(height < 420px)").addEventListener("change", e => {
   const nav = document.getElementById("nav");
   const unofficial = document.getElementById("unofficial");
 
-  if (window.innerHeight < 420) {
+  if (e.matches) {
     nav.style.top = "";  // hide
   } else if (unofficial.getBoundingClientRect().bottom > 0) {
     nav.style.top = "";  // hide
   } else {
     nav.style.top = "0px";  // show
   }
-}));
+});
 
 new IntersectionObserver(entries => {
   const nav = document.getElementById("nav");
