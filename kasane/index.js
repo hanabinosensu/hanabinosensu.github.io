7f9aa0899ce963094224e64fd4e30dae4eb5fd3b
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2026-04-13 05:01:48 +0900

    added dedicated page for kasane

---
+++ kasane/index.js
@@ -0,0 +1,26 @@
+/* Show #nav iff window height >= 420px and #unofficial is outside viewport */
+window.matchMedia("(height < 420px)").addEventListener("change", e => {
+  const nav = document.getElementById("nav");
+  const unofficial = document.getElementById("unofficial");
+
+  if (e.matches) {
+    nav.style.top = "";  // hide
+  } else if (unofficial.getBoundingClientRect().bottom > 0) {
+    nav.style.top = "";  // hide
+  } else {
+    nav.style.top = "0px";  // show
+  }
+});
+
+new IntersectionObserver(entries => {
+  const nav = document.getElementById("nav");
+  const unofficial = entries[0];
+
+  if (window.innerHeight < 420) {
+    nav.style.top = "";  // hide
+  } else if (unofficial.isIntersecting) {
+    nav.style.top = "";  // hide
+  } else {
+    nav.style.top = "0px";  // show
+  }
+}, {}).observe(document.getElementById("unofficial"));
