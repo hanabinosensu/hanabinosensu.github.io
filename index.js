ca9e089a8f4c39e7cc653c99de66cff18477e777
hanabinosensu <187201503+hanabinosensu@users.noreply.github.com>
2026-08-29 09:09:14 +0900

    live update #next during harajuku-2026

--- index.js
+++ index.js
@@ -30,6 +30,36 @@ if (!navigator.languages.includes("ja")) {
   document.getElementById("i18n").style.display = "block";
 }
 
+/* Live update #next */
+document.getElementById("next").addEventListener("animationstart", e => {
+  const next = e.target;
+  const now = Date.now();
+
+  if (now < Date.parse("2026-08-29T08:00+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">原宿表参道元氣祭 （08/29–30）</a>`;
+  } else if (now < Date.parse("2026-08-29T11:25+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">NHK前ストリート （11:20）</a>`;
+  } else if (now < Date.parse("2026-08-29T13:47+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">神宮会館ステージ （13:42）</a>`;
+  } else if (now < Date.parse("2026-08-29T16:05+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">原宿口ステージ （16:00）</a>`;
+  } else if (now < Date.parse("2026-08-29T18:11+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">代々木公園ステージ （18:06）</a>`;
+  } else if (now < Date.parse("2026-08-30T08:00+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">原宿表参道元氣祭 （08/29–30）</a>`;
+  } else if (now < Date.parse("2026-08-30T11:30+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">表参道アヴェニュー （11:20）</a>`;
+  } else if (now < Date.parse("2026-08-30T12:59+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">代々木公園 BE STAGE （12:54）</a>`;
+  } else if (now < Date.parse("2026-08-30T16:23+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">代々木公園ステージ （16:18）</a>`;
+  } else if (now < Date.parse("2026-08-30T18:00+09:00")) {
+    next.innerHTML = `次: <a href="#harajuku-2026">原宿表参道元氣祭 （08/29–30）</a>`;
+  } else {
+    next.innerHTML = `次: にいがた総踊り （09/20–21）`;
+  }
+});
+
 /* Infinite #topics slideshow */
 document.getElementById("topics").scrollTo(0, 0);
 
